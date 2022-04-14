ec2ab90b903f8a2c8f8692d41d7c3e16458d155c
Petr Šmejkal <smejkp13@fit.cvut.cz>
2022-04-14 08:58:35 +0200

    added comments for terminal module

--- src/Modules/Terminal.hpp
+++ src/Modules/Terminal.hpp
@@ -16,21 +16,83 @@
 namespace libANSI {
     class Terminal {
     public:
+        /**
+         * @brief Puts the terminal into raw mode.
+         * 
+         */
         void rawMode();
+
+        /**
+         * @brief Puts the terminal into cooked mode.
+         * 
+         */
         void cookedMode();
         
+
+        /**
+         * @brief Resizes the terminal window to given width and height.
+         * 
+         * @param w Width the terminal should be resized to.
+         * @param h Height the terminal should be resized to.
+         */
         void resize(int w, int h);
+
+        /**
+         * @brief Minimalizes the terminal window (puts it down to the dock menu bar).
+         * 
+         */
         void minimize();
+
+        /**
+         * @brief Maximizes the terminal window (makes it the active window).
+         * 
+         */
         void maximize();
         
+
+        /**
+         * @brief Clears the entire terminal area.
+         * 
+         */
         void clearScreen();
+
+        /**
+         * @brief Clears the terminal area from the current cursor position to the begging (top left corner).
+         * 
+         */
         void clearScreenToBeggining();
+
+        /**
+         * @brief Clears the terminal area from the current cursor position to the end (bottom right corner).
+         * 
+         */
         void clearScreenToEnd();
         
+        /**
+         * @brief Sets background color for the terminal window.
+         * 
+         * @param color Value of enum COLOR (for standard color chose from COLOR::FG_*; for bright color choose from COLOR::BG_*).
+         */
         void backgroundColor(COLOR color);
+
+        /**
+         * @brief Sets background color for the terminal window.
+         * 
+         * @param bright_color Value of enum COLOR (for standard color chose from COLOR::FG_*; for bright color choose from COLOR::BG_*).
+         */
         void backgroundBrightColor(COLOR bright_color);
+
+        /**
+         * @brief Sets background color for the terminal window.
+         * 
+         * @param color_code Int value in range 0-255. May not be supported on all terminals.
+         */
         void backgroundColor(int color_code);
         
+        /**
+         * @brief Resets all previously set terminal modifiers.
+         * 
+         */
         void reset();
     private:
     };
